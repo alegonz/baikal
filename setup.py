cb58e413d0920bfbccc82487ab6e092611b09446
alegonz <alejandrojgt@gmail.com>
2018-11-11 13:19:31 +0900

    Added setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+#
+# Copyright (c) 2018 Alejandro González Tineo <alejandrojgt@gmail.com>
+# License: New 3-clause BSD
+
+from setuptools import setup
+
+
+version = '0.1.0'
+
+
+setup(name='baikal',
+      version=version,
+      description='Graph-based functional API for building machine learning pipelines',
+      url='https://gitlab.com/alegonz/baikal',
+      license='new BSD',
+      author='Alejandro González Tineo',
+      author_email='alejandrojgt@gmail.com',
+      packages=['baikal'])
