f9a6bac4cdbf0aea8dcc42417848b2e68c31701a
alegonz <alejandrojgt@gmail.com>
2019-02-03 20:16:12 +0900

    Exposed build_output_shapes instead of build_outputs. Removede graph name from node/data names.

--- baikal/core.py
+++ baikal/core.py
@@ -1,4 +1,4 @@
-from typing import Union, List
+from typing import Union, List, Tuple
 
 
 def make_name(*parts, sep='/'):
@@ -11,7 +11,7 @@ class DiGraph:
         self.name = name
 
     def add_node(self, node):
-        self._adjacency[node] = set()  # successors (TODO: perhaps should predecessors)
+        self._adjacency[node] = set()  # successors (TODO: perhaps should be predecessors)
 
     def add_edge(self, from_node, to_node):
         self._adjacency[from_node].add(to_node)
@@ -44,26 +44,28 @@ class Data:
         self.name = make_name(node.name, index)
 
 
-# TODO: '/0' at the end is cumbersome and unnecessary in InputNode's
-# TODO: Consider removing the graph name from the node name
 class Node:
+    # used to keep track of number of instances and make unique names
+    # a dict-of-dicts with graph and name as keys.
     _names = dict()
 
     def __init__(self, name=None):
         # Maybe graph should be passed as a keyword argument
-        self.graph = default_graph
+        graph = default_graph
+        self.graph = graph
         self.graph.add_node(self)
+
+        if graph not in self._names:
+            self._names[graph] = {}
 
         if name is None:
             name = self.__class__.__name__
 
-        name = make_name(self.graph.name, name)
-
-        n_instances = self._names.get(name, 0)
-        self.name = '_'.join([name, str(n_instances)])
+        n_instances = self._names[graph].get(name, 0)
+        self.name = make_name(name, n_instances, sep='_')
 
         n_instances += 1
-        self._names[name] = n_instances
+        self._names[graph][name] = n_instances
 
     @classmethod
     def clear_names(cls):
@@ -88,24 +90,33 @@ class ProcessorMixin(Node):
             predecessor = input.node
             self.graph.add_edge(predecessor, self)
 
-        self.outputs = self.build_outputs(inputs)
+        self.outputs = self._build_outputs(inputs)
         return self.outputs
 
     # TODO: We might need a check_inputs method as well (Concatenate, Split, Merge, etc will need it).
     # Also, sklearn-based Processors can accept only shapes of length 1
     # (ignoring the samples, the dimensionality of the feature vector)
 
-    def build_outputs(self, inputs: List[Data]) -> Union[Data, List[Data]]:
-        # TODO: Maybe should be build_output_shapes.
-        # It is not necessary to make the user import and use Data when extending this abstract mixin
+    def _build_outputs(self, inputs: List[Data]) -> Union[Data, List[Data]]:
+        input_shapes = [input.shape for input in inputs]
+        output_shapes = self.build_output_shapes(input_shapes)
+
+        if len(output_shapes) == 1:
+            return Data(output_shapes[0], self, 0)
+
+        return [Data(shape, self, i) for i, shape in enumerate(output_shapes)]
+
+    def build_output_shapes(self, input_shapes: List[Tuple]) -> List[Tuple]:
         raise NotImplementedError
 
-    # TODO: add compute abstract method
+    def compute(self, *inputs):
+        raise NotImplementedError
 
 
 class InputNode(Node):
     def __init__(self, shape, name=None):
         super().__init__(name)
+        # TODO: Maybe '/0' at the end is cumbersome and unnecessary in InputNode's
         self.outputs = Data(shape, self)
 
 
@@ -113,4 +124,3 @@ def Input(shape, name=None):
     # Maybe this can be implemented in InputNode.__new__
     input = InputNode(shape, name)
     return input.outputs
-
--- tests/test_core.py
+++ tests/test_core.py
@@ -17,24 +17,24 @@ class TestInput:
 
         node = default_graph.nodes[0]
         assert isinstance(node, InputNode)
-        assert 'default/InputNode_0' == node.name
+        assert 'InputNode_0' == node.name
         assert isinstance(x0, Data)
         assert (10,) == x0.shape
-        assert 'default/InputNode_0/0' == x0.name
+        assert 'InputNode_0/0' == x0.name
 
     def test_instantiate_two_with_same_name(self, teardown):
         x0 = Input((5,), name='x')
         x1 = Input((2,), name='x')
 
-        assert 'default/x_0/0' == x0.name
-        assert 'default/x_1/0' == x1.name
+        assert 'x_0/0' == x0.name
+        assert 'x_1/0' == x1.name
 
     def test_instantiate_two_without_name(self, teardown):
         x0 = Input((5,))
         x1 = Input((2,))
 
-        assert 'default/InputNode_0/0' == x0.name
-        assert 'default/InputNode_1/0' == x1.name
+        assert 'InputNode_0/0' == x0.name
+        assert 'InputNode_1/0' == x1.name
 
 
 class TestProcessorMixin:
@@ -43,9 +43,11 @@ class TestProcessorMixin:
     @pytest.fixture(scope='class')
     def extended_sklearn_class(cls):
         class LogisticRegression(ProcessorMixin, sklearn.linear_model.logistic.LogisticRegression):
-            # TODO: add compute method
-            def build_outputs(self, inputs):
-                return Data((1,), self)
+            def build_output_shapes(self, input_shapes):
+                return [(1,)]
+
+            def compute(self, x):
+                return self.predict(x)
 
         return LogisticRegression
 
@@ -55,13 +57,12 @@ class TestProcessorMixin:
 
         assert isinstance(y, Data)
         assert (1,) == y.shape
-        assert 'default/LogisticRegression_0/0' == y.name
+        assert 'LogisticRegression_0/0' == y.name
 
     def test_call_with_two_inputs(self, teardown):
         class MIMOProcessor(ProcessorMixin):
-            # TODO: add compute method
-            def build_outputs(self, inputs):
-                return Data((1,), self, 0), Data((1,), self, 1)
+            def build_output_shapes(self, input_shapes):
+                return [(1,), (1,)]
 
         x0 = Input((10,), name='x')
         x1 = Input((10,), name='x')
@@ -71,21 +72,21 @@ class TestProcessorMixin:
         assert isinstance(y1, Data)
         assert (1,) == y0.shape
         assert (1,) == y1.shape
-        assert 'default/MIMOProcessor_0/0' == y0.name
-        assert 'default/MIMOProcessor_0/1' == y1.name
+        assert 'MIMOProcessor_0/0' == y0.name
+        assert 'MIMOProcessor_0/1' == y1.name
 
     def test_instantiate_two_with_same_name(self, extended_sklearn_class, teardown):
         x = Input((10,), name='x')
         y0 = extended_sklearn_class(name='myclassifier')(x)
         y1 = extended_sklearn_class(name='myclassifier')(x)
 
-        assert 'default/myclassifier_0/0' == y0.name
-        assert 'default/myclassifier_1/0' == y1.name
+        assert 'myclassifier_0/0' == y0.name
+        assert 'myclassifier_1/0' == y1.name
 
     def test_instantiate_two_without_name(self, extended_sklearn_class, teardown):
         x = Input((10,), name='x')
         y0 = extended_sklearn_class()(x)
         y1 = extended_sklearn_class()(x)
 
-        assert 'default/LogisticRegression_0/0' == y0.name
-        assert 'default/LogisticRegression_1/0' == y1.name
+        assert 'LogisticRegression_0/0' == y0.name
+        assert 'LogisticRegression_1/0' == y1.name
