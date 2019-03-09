e7ff3c68aaac233b43df468a9d16eeda4cf207f6
alegonz <alejandrojgt@gmail.com>
2019-03-09 19:04:15 +0900

    Added more tests to TestModel class. Removed *args and refactored slearn_classifier_step factory.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,10 @@
 import pytest
 import numpy as np
-import sklearn.linear_model.logistic
 from sklearn import datasets
+import sklearn.decomposition
+import sklearn.linear_model.logistic
+from sklearn.exceptions import NotFittedError
+from sklearn.utils.validation import check_is_fitted
 
 from baikal.core.data import Data
 from baikal.core.digraph import default_graph, Node, DiGraph
@@ -40,10 +43,10 @@ class TestInput:
 
 
 @pytest.fixture
-def extended_sklearn_class():
+def sklearn_classifier_step():
     class LogisticRegression(Step, sklearn.linear_model.logistic.LogisticRegression):
-        def __init__(self, *args, name=None, **kwargs):
-            super(LogisticRegression, self).__init__(*args, name=name, **kwargs)
+        def __init__(self, name=None, **kwargs):
+            super(LogisticRegression, self).__init__(name=name, **kwargs)
 
         def build_output_shapes(self, input_shapes):
             return [(1,)]
@@ -51,14 +54,45 @@ def extended_sklearn_class():
         def compute(self, x):
             return self.predict(x)
 
+        @property
+        def fitted(self):
+            try:
+                return check_is_fitted(self, ['coef_'], all_or_any=all)
+            except NotFittedError:
+                return False
+
     return LogisticRegression
+
+
+@pytest.fixture
+def sklearn_transformer_step():
+    class PCA(Step, sklearn.decomposition.PCA):
+        def __init__(self, name=None, **kwargs):
+            super(PCA, self).__init__(name=name, **kwargs)
+
+        def build_output_shapes(self, input_shapes):
+            # TODO: How to handle when n_components is determined during fit?
+            # This occurs when passed as a percentage of total variance (0 < n_components < 1)
+            return [(self.n_components,)]
+
+        def compute(self, x):
+            return self.transform(x)
+
+        @property
+        def fitted(self):
+            try:
+                return check_is_fitted(self, ['mean_', 'components_'], all_or_any=all)
+            except NotFittedError:
+                return False
+
+    return PCA
 
 
 class TestStep:
 
-    def test_call(self, extended_sklearn_class, teardown):
+    def test_call(self, sklearn_classifier_step, teardown):
         x = Input((10,), name='x')
-        y = extended_sklearn_class()(x)
+        y = sklearn_classifier_step()(x)
 
         assert isinstance(y, Data)
         assert (1,) == y.shape
@@ -80,52 +114,75 @@ class TestStep:
         assert 'MIMOStep_0/0' == y0.name
         assert 'MIMOStep_0/1' == y1.name
 
-    def test_instantiate_two_with_same_name(self, extended_sklearn_class, teardown):
+    def test_instantiate_two_with_same_name(self, sklearn_classifier_step, teardown):
         x = Input((10,), name='x')
-        y0 = extended_sklearn_class(name='myclassifier')(x)
-        y1 = extended_sklearn_class(name='myclassifier')(x)
+        y0 = sklearn_classifier_step(name='myclassifier')(x)
+        y1 = sklearn_classifier_step(name='myclassifier')(x)
 
         assert 'myclassifier_0/0' == y0.name
         assert 'myclassifier_1/0' == y1.name
 
-    def test_instantiate_two_without_name(self, extended_sklearn_class, teardown):
+    def test_instantiate_two_without_name(self, sklearn_classifier_step, teardown):
         x = Input((10,), name='x')
-        y0 = extended_sklearn_class()(x)
-        y1 = extended_sklearn_class()(x)
+        y0 = sklearn_classifier_step()(x)
+        y1 = sklearn_classifier_step()(x)
 
         assert 'LogisticRegression_0/0' == y0.name
         assert 'LogisticRegression_1/0' == y1.name
 
 
 class TestModel:
-    def test_instantiation(self, extended_sklearn_class, teardown):
+    def test_instantiation(self, sklearn_classifier_step, teardown):
         x = Input((10,), name='x')
-        y = extended_sklearn_class()(x)
+        y = sklearn_classifier_step()(x)
         model = Model(x, y)
 
-    def test_instantiation_with_wrong_input_type(self, extended_sklearn_class, teardown):
+    def test_instantiation_with_wrong_input_type(self, sklearn_classifier_step, teardown):
         x = Input((10,), name='x')
-        y = extended_sklearn_class()(x)
+        y = sklearn_classifier_step()(x)
 
         x_wrong = np.zeros((10,))
         with pytest.raises(ValueError):
             model = Model(x_wrong, y)
 
-    def test_fit(self, extended_sklearn_class, teardown):
+    def test_fit_classifier(self, sklearn_classifier_step, teardown):
         # Based on the example in
         # https://scikit-learn.org/stable/auto_examples/linear_model/plot_iris_logistic.html
         iris = datasets.load_iris()
 
-        X = iris.data[:, :2]  # we only take the first two features.
-        y = iris.target
+        X_data = iris.data[:, :2]  # we only take the first two features.
+        y_data = iris.target
 
         x = Input((2,), name='x')
-        yp = extended_sklearn_class(multi_class='multinomial')(x)
+        y = sklearn_classifier_step(multi_class='multinomial')(x)
 
-        model = Model(x, yp)
-        model.fit(X, y)
+        model = Model(x, y)
 
-        assert hasattr(extended_sklearn_class, 'coef_')
+        # Style 1: pass data as in instantiation
+        model.fit(X_data, y_data)
+        assert y.node.fitted
+
+        # FIXME: These will likely pass if the above passes.
+        # Split into separate tests (init a new model everytime)
+
+        # Style 2: pass a dict keyed by Data instances
+        # model.fit({x: X_data, y: y_data})
+        # assert y.node.fitted
+        #
+        # # Style 3: pass a dict keyed by Data instances names
+        # model.fit({'x': X_data, 'LogisticRegression_0/0': y_data})
+        # assert y.node.fitted
+
+    def test_fit_transformer(self, sklearn_transformer_step, teardown):
+        iris = datasets.load_iris()
+        X_data = iris.data
+
+        x = Input((4,), name='x')
+        xt = sklearn_transformer_step(n_components=2)(x)
+
+        model = Model(x, xt)
+        model.fit(X_data)
+        assert y.node.fitted
 
 
 class TestDiGraph:
