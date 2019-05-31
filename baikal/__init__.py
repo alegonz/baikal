ee9f97de15b5387f96938903e58426716c6d3167
alegonz <alejandrojgt@gmail.com>
2019-06-01 08:56:07 +0900

    Added _version.py

--- baikal/__init__.py
+++ baikal/__init__.py
@@ -1,3 +1,5 @@
+from baikal._version import __version__
+
 # Make the most relevant classes importable from root
 from baikal._core.model import Model
 from baikal._core.step import Step, Input
---
+++ baikal/_version.py
@@ -0,0 +1,3 @@
+# This module is imported from __init__.py and executed from setup.py
+
+__version__ = '0.1.0.dev'
