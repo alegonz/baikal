4e033d89c313187bd5760cefce4f658eb3bd5815
Alejandro Gonzalez <alejandrojgt@gmail.com>
2019-11-07 00:49:01 +0900

    Add Split step

--- baikal/steps/__init__.py
+++ baikal/steps/__init__.py
@@ -1,5 +1,5 @@
 from baikal._core.step import Step, Input
 
 from baikal.steps.expression import Lambda
-from baikal.steps.merge import ColumnStack, Concatenate, Stack
+from baikal.steps.merge import ColumnStack, Concatenate, Stack, Split
 from baikal.steps.factory import make_step
