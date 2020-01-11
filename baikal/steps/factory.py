00a8f38148a036af9e795d58394a15726790c86f
Alejandro Gonzalez <alejandrojgt@gmail.com>
2020-01-11 16:55:54 +0900

    Add attrs_dict to make_step to add custom attributes to steps

--- baikal/steps/factory.py
+++ baikal/steps/factory.py
@@ -3,7 +3,7 @@ import inspect
 from baikal import Step
 
 
-def make_step(base_class):
+def make_step(base_class, attr_dict=None):
     """Creates a step subclass from the given base class.
 
     For example, calling
@@ -21,10 +21,16 @@ def make_step(base_class):
     base_class : type
         The base class to inherit from. It must implement the scikit-learn API.
 
+    attr_dict : dict
+        Dictionary of additional attributes for the class. You can use this to add
+        methods such as `fit_compute` to the class. (keys: name of attribute (str),
+        values: attributes).
+
     Returns
     -------
     step_subclass: type
-        A new class that inherits from both Step and the given base class.
+        A new class that inherits from both Step and the given base class and has the
+        the specified attributes.
 
     """
 
@@ -37,7 +43,11 @@ def make_step(base_class):
     name = base_class.__name__
     bases = (Step, base_class)
     caller_module = inspect.currentframe().f_back.f_globals["__name__"]
+
     dict = {"__init__": __init__, "__module__": caller_module}
+    if attr_dict is not None:
+        dict.update(attr_dict)
+
     step_subclass = metaclass(name, bases, dict)
 
     return step_subclass
