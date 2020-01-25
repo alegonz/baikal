a8e2ad496cbc9c2762b17e1f081031ea3c8cbfb0
Alejandro Gonzalez <alejandrojgt@gmail.com>
2020-01-26 01:54:25 +0900

    Add test_plot_nested

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -5,6 +5,7 @@ from baikal.plot import plot_model
 
 from tests.helpers.fixtures import teardown
 from tests.helpers.dummy_steps import DummyMIMO, DummySIMO, DummySISO, DummyMISO
+from tests.helpers.sklearn_steps import LogisticRegression
 
 
 @pytest.mark.parametrize("expand_nested", [True, False])
@@ -40,6 +41,27 @@ def test_plot_model(teardown, tmp_path, expand_nested):
     model = Model([x, u], [w, v], y_t, name="main_model")
 
     filename = str(tmp_path / "test_plot_model.png")
-    plot_model(
-        model, filename, show=False, expand_nested=expand_nested,
-    )
+    plot_model(model, filename, show=False, expand_nested=expand_nested)
+
+
+@pytest.mark.parametrize("levels", [0, 1, 2, 3])
+@pytest.mark.parametrize("expand_nested", [True, False])
+def test_plot_nested(teardown, tmp_path, levels, expand_nested):
+    def build_model(step, level):
+        x = Input(name="x_sub{}".format(level))
+        y_t = Input(name="y_t_sub{}".format(level))
+        y_p = step(x, y_t)
+        return Model(x, y_p, y_t)
+
+    sub_models = [LogisticRegression()]
+    for level in range(levels):
+        sub_model = build_model(sub_models[level], level + 1)
+        sub_models.append(sub_model)
+
+    x = Input(name="x")
+    y_t = Input(name="y_t")
+    y_p = sub_models[-1](x, y_t)
+    model = Model(x, y_p, y_t)
+
+    filename = str(tmp_path / "test_plot_model.png")
+    plot_model(model, filename, show=False, expand_nested=expand_nested)
