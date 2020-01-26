827e4bb0f0bf447162025264a24ab9b57956da05
Alejandro Gonzalez <alejandrojgt@gmail.com>
2020-01-26 16:57:42 +0900

    Add another submodel to test_plot_big_model

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -18,38 +18,6 @@ def build_submodel(step, number):
 
 
 @pytest.mark.parametrize("expand_nested", [True, False])
-def test_plot_model(teardown, tmp_path, expand_nested):
-    # Below is a very contrived dummy model
-
-    # ------- Sub-model 1
-    x1_sub1 = Input(name="x1_sub1")
-    x2_sub1 = Input(name="x2_sub1")
-    y_t_sub1 = Input(name="y_t_sub1")
-    y_p1_sub1, y_p2_sub1 = DummyMIMO()([x1_sub1, x2_sub1], y_t_sub1)
-    submodel1 = Model(
-        [x1_sub1, x2_sub1], [y_p1_sub1, y_p2_sub1], y_t_sub1, name="submodel1"
-    )
-
-    # ------- Sub-model 2
-    y_t_sub2 = Input(name="y_t_sub2")
-    y_p_sub2 = DummySISO()(y_t_sub2)
-    submodel2 = Model(y_t_sub2, y_p_sub2, name="submodel2")
-
-    # ------- Main model
-    x = Input(name="x")
-    y_t = Input(name="y_t")
-    y_t_trans = submodel2(y_t)
-    h1, h2 = DummySIMO()(x)
-    z1, z2 = submodel1([h1, h2], y_t_trans)
-    w = DummyMISO()([z1, z2])
-
-    model = Model(x, w, y_t, name="main_model")
-
-    filename = str(tmp_path / "test_plot_model.png")
-    plot_model(model, filename, show=False, expand_nested=expand_nested)
-
-
-@pytest.mark.parametrize("expand_nested", [True, False])
 def test_plot_independent_submodels(teardown, tmp_path, expand_nested):
     xs, y_ts, y_ps, steps = [], [], [], []
 
@@ -102,6 +70,45 @@ def test_plot_nested_submodels(teardown, tmp_path, levels, expand_nested):
     y_t = Input(name="y_t")
     y_p = submodels[-1](x, y_t)
     model = Model(x, y_p, y_t)
+
+    filename = str(tmp_path / "test_plot_model.png")
+    plot_model(model, filename, show=False, expand_nested=expand_nested)
+
+
+@pytest.mark.parametrize("expand_nested", [True, False])
+def test_plot_big_model(teardown, tmp_path, expand_nested):
+    # Below is a very contrived big dummy model
+
+    # ------- Sub-model 1
+    x1_sub1 = Input(name="x1_sub1")
+    x2_sub1 = Input(name="x2_sub1")
+    y_t_sub1 = Input(name="y_t_sub1")
+    y_p1_sub1, y_p2_sub1 = DummyMIMO()([x1_sub1, x2_sub1], y_t_sub1)
+    submodel1 = Model(
+        [x1_sub1, x2_sub1], [y_p1_sub1, y_p2_sub1], y_t_sub1, name="submodel1"
+    )
+
+    # ------- Sub-model 2
+    y_t_sub2 = Input(name="y_t_sub2")
+    y_p_sub2 = DummySISO()(y_t_sub2)
+    submodel2 = Model(y_t_sub2, y_p_sub2, name="submodel2")
+
+    # ------- Sub-model 3
+    x1_sub3 = Input(name="x1_sub3")
+    x2_sub3 = Input(name="x2_sub3")
+    y_p1_sub3, y_p2_sub3 = DummyMIMO()([x1_sub3, x2_sub3])
+    submodel3 = Model([x1_sub3, x2_sub3], [y_p1_sub3, y_p2_sub3], name="submodel3")
+
+    # ------- Main model
+    x = Input(name="x")
+    y_t = Input(name="y_t")
+    y_t_trans = submodel2(y_t)
+    h1, h2 = DummySIMO()(x)
+    g1, g2 = submodel3([h1, h2])
+    z1, z2 = submodel1([g1, g2], y_t_trans)
+    w = DummyMISO()([z1, z2])
+
+    model = Model(x, w, y_t, name="main_model")
 
     filename = str(tmp_path / "test_plot_model.png")
     plot_model(model, filename, show=False, expand_nested=expand_nested)
