alegonz/baikal

commit 3e69de5769a181f99c020b221d4725083a262406

fit/predict args must be either a single-item or a list
This is necessary to be compatible with sklearn steps and also
with nested baikal models.

baikal/steps/merge.py
@@ -10,7 +10,7 @@ class Concatenate(Step):
         super().__init__(name=name)
         self.axis = axis
 
-    def transform(self, *Xs):
+    def transform(self, Xs):
         return np.concatenate(Xs, axis=self.axis)
 
 
@@ -21,7 +21,7 @@ class Stack(Step):
         super().__init__(name=name)
         self.axis = axis
 
-    def transform(self, *Xs):
+    def transform(self, Xs):
         return np.stack(Xs, axis=self.axis)
 
 
@@ -31,7 +31,7 @@ class ColumnStack(Step):
     def __init__(self, name=None):
         super().__init__(name=name)
 
-    def transform(self, *Xs):
+    def transform(self, Xs):
         return np.column_stack(Xs)
 
 

baikal/_core/model.py
@@ -6,7 +6,7 @@ from baikal._core.data_placeholder import is_data_placeholder_list, DataPlacehol
 from baikal._core.digraph import DiGraph
 from baikal._core.step import Step, InputStep, Node
 from baikal._core.typing import ArrayLike
-from baikal._core.utils import listify, safezip2, SimpleCache
+from baikal._core.utils import listify, safezip2, SimpleCache, unlistify
 
 # Just to avoid function signatures painful to the eye
 DataPlaceHolders = Union[DataPlaceholder, List[DataPlaceholder]]
@@ -412,7 +412,10 @@ class Model(Step):
                 fit_params = fit_params_steps.get(node.step, {})
 
                 # TODO: Add a try/except to catch missing output data errors (e.g. when forgot ensemble outputs)
-                node.step.fit(*Xs, *ys, **fit_params)
+                if ys:
+                    node.step.fit(unlistify(Xs), unlistify(ys), **fit_params)
+                else:
+                    node.step.fit(unlistify(Xs), **fit_params)
 
             # 2) predict/transform phase
             if list(self.graph.successors(node)):
@@ -486,7 +489,7 @@ class Model(Step):
         # This happens when recomputing a step that had a subset of its outputs already passed in the inputs.
         # TODO: Some regressors have extra options in their predict method, and they return a tuple of arrays.
         # https://scikit-learn.org/stable/glossary.html#term-predict
-        output_data = node.compute_func(*Xs)
+        output_data = node.compute_func(unlistify(Xs))
         output_data = listify(output_data)
 
         try:

baikal/steps/expression.py
@@ -13,9 +13,10 @@ class Lambda(Step):
     Parameters
     ----------
     compute_func
-        The function to make the step from. This function takes one or several input
-        data object as its first arguments. If compute_func takes additional arguments
-        you may either pass them as keyword arguments or use a functools.partial object.
+        The function to make the step from. This function a single array-like object
+        (in the case of a single input) or a list of array-like objects (in the case of
+        multiple inputs) If compute_func takes additional arguments you may either pass
+        them as keyword arguments or use a functools.partial object.
 
     n_outputs
         Number of outputs of the function.
@@ -29,7 +30,8 @@ class Lambda(Step):
 
     Examples
     --------
-    >>> def function(x1, x2):
+    >>> def function(Xs):
+    >>>     x1, x2 = Xs
     >>>     return 2 * x1, x2 / 2
     >>>
     >>> x = Input()

tests/_core/test_model.py
@@ -329,6 +329,22 @@ class TestFit:
         model.fit(np.array([1, 3, 1, 3]).reshape(-1, 1))
         assert (scaler.mean_, scaler.var_) == (2.0, 1.0)
 
+    def test_fit_nested_model_with_multiple_inputs(self, teardown):
+        def build_model(step):
+            x1 = Input()
+            x2 = Input()
+            y_t1 = Input()
+            y_t2 = Input()
+            y_p = step([x1, x2], [y_t1, y_t2])
+            return Model([x1, x2], y_p, [y_t1, y_t2])
+
+        step = DummyMISO()
+        submodel = build_model(step)
+        model = build_model(submodel)
+
+        model.fit([iris.data, iris.data], [iris.target, iris.target])
+        assert step.fitted_
+
 
 class TestPredict:
     x1_data = iris.data[:, :2]
@@ -574,18 +590,15 @@ def test_fit_predict_with_shared_step(teardown):
 
 
 def test_fit_and_predict_model_with_no_fittable_steps(teardown):
-    X1_data = np.array([[1, 2], [3, 4]])
-    X2_data = np.array([[5, 6], [7, 8]])
-    y_expected = np.array([[12, 16], [20, 24]])
+    X_data = np.array([[1, 2], [3, 4]])
+    y_expected = np.array([[2, 4], [6, 8]])
 
-    x1 = Input()
-    x2 = Input()
-    z = DummyMISO()([x1, x2])
-    y = DummySISO()(z)
+    x = Input()
+    y = DummySISO()(x)
 
-    model = Model([x1, x2], y)
-    model.fit([X1_data, X2_data])  # nothing to fit
-    y_pred = model.predict([X1_data, X2_data])
+    model = Model(x, y)
+    model.fit(X_data)  # nothing to fit
+    y_pred = model.predict(X_data)
 
     assert_array_equal(y_pred, y_expected)
 

tests/helpers/dummy_steps.py
@@ -10,8 +10,8 @@ class DummySISO(Step):
     def __init__(self, name=None):
         super().__init__(name=name)
 
-    def transform(self, x):
-        return 2 * x
+    def transform(self, X):
+        return 2 * X
 
 
 class DummySIMO(Step):
@@ -21,8 +21,8 @@ class DummySIMO(Step):
     def __init__(self, name=None):
         super().__init__(name=name, n_outputs=2)
 
-    def transform(self, x):
-        return x + 1.0, x - 1.0
+    def transform(self, X):
+        return X + 1.0, X - 1.0
 
 
 class DummyMISO(Step):
@@ -32,8 +32,14 @@ class DummyMISO(Step):
     def __init__(self, name=None):
         super().__init__(name=name)
 
-    def transform(self, x1, x2):
+    def transform(self, Xs):
+        x1, x2 = Xs
         return x1 + x2
+
+    def fit(self, Xs, ys):
+        # Suppose that this dummy model expects two inputs and two targets
+        self.fitted_ = True
+        return self
 
 
 class DummyMIMO(Step):
@@ -43,7 +49,8 @@ class DummyMIMO(Step):
     def __init__(self, name=None):
         super().__init__(name=name, n_outputs=2)
 
-    def transform(self, x1, x2):
+    def transform(self, Xs):
+        x1, x2 = Xs
         return x1 * 10.0, x2 / 10.0
 
     def fit(self, X):
@@ -57,8 +64,8 @@ class DummyImproperlyDefined(Step):
     def __init__(self, name=None):
         super().__init__(name=name)
 
-    def transform(self, x):
-        return x + 1.0, x - 1.0
+    def transform(self, X):
+        return X + 1.0, X - 1.0
 
 
 class _DummyEstimator(BaseEstimator):

tests/steps/test_expression.py
@@ -8,7 +8,8 @@ from tests.helpers.fixtures import teardown
 
 
 def test_lambda(teardown):
-    def compute_func(x1, x2, p1, p2=1):
+    def compute_func(Xs, p1, p2=1):
+        x1, x2 = Xs
         return p1 * x1, x2 / p2
 
     x = Input()

commit f51676ded2674720b9b6d72d35b3fbcb6bb17686

Listify inputs of merge steps for idempotency
This fixes a bug introduced in 3e69de5

baikal/steps/merge.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from baikal._core.step import Step
+from baikal._core.utils import listify as _listify
 
 
 class Concatenate(Step):
@@ -11,7 +12,7 @@ class Concatenate(Step):
         self.axis = axis
 
     def transform(self, Xs):
-        return np.concatenate(Xs, axis=self.axis)
+        return np.concatenate(_listify(Xs), axis=self.axis)
 
 
 class Stack(Step):
@@ -22,7 +23,7 @@ class Stack(Step):
         self.axis = axis
 
     def transform(self, Xs):
-        return np.stack(Xs, axis=self.axis)
+        return np.stack(_listify(Xs), axis=self.axis)
 
 
 class ColumnStack(Step):
@@ -32,7 +33,7 @@ class ColumnStack(Step):
         super().__init__(name=name)
 
     def transform(self, Xs):
-        return np.column_stack(Xs)
+        return np.column_stack(_listify(Xs))
 
 
 class Split(Step):

tests/steps/test_merge.py
@@ -67,3 +67,16 @@ def test_split(x, indices_or_sections, teardown):
 
     for actual, expected in safezip2(y_pred, y_expected):
         assert_array_equal(actual, expected)
+
+
+@pytest.mark.parametrize("step_class", [ColumnStack, Concatenate, Stack])
+def test_single_input(step_class, teardown):
+    x = Input()
+    y = step_class()(x)
+    model = Model(x, y)
+
+    x_data = np.array([[1, 2], [3, 4]])
+    if step_class is Stack:
+        assert_array_equal(x_data.reshape((2, 2, 1)), model.predict(x_data))
+    else:
+        assert_array_equal(x_data, model.predict(x_data))