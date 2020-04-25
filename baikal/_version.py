56857b62ec94ad51152a31ece92894737ddde61f
Alejandro Gonzalez <alejandrojgt@gmail.com>
2020-04-26 00:12:27 +0900

    v0.3.1

--- baikal/_version.py
+++ baikal/_version.py
@@ -1,4 +1,4 @@
 # This module is imported from __init__.py and executed from setup.py
 # master (development) branch format must be in MAJOR.MINOR-dev
 # release branches format must be MAJOR.MINOR.PATCH
-__version__ = "0.3.0"
+__version__ = "0.3.1"
